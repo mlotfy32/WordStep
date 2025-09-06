6b933c6c05f2f35beacecaaed08c00093fc6efdf
mhmd <mhmdlotdy368@gmail.com>
2025-09-06 03:51:24 +0300

    add sharedpref

---
+++ lib/core/utiles/shared_pref/prefs_Keys.dart
@@ -0,0 +1,8 @@
+class PrefsKeys {
+  static const String isLogin = "isLogin";
+  static const String accessToken = "accessToken";
+  static const String email = "email";
+  static const String totalHoursCource = "totalHoures";
+  static const String totalHoursCources = "totalHoursCources";
+  static const String userImage = "userImage";
+}
