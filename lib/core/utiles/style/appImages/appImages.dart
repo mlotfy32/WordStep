1dec236d6d2500c6f25f30791550735c3c7052e6
mhmd <mhmdlotdy368@gmail.com>
2025-09-06 03:50:12 +0300

    add app styles

---
+++ lib/core/utiles/style/appImages/appImages.dart
@@ -0,0 +1,26 @@
+class Appimages {
+  static const String loginBacGround = 'assets/images/auth/loginBackGround.png';
+  static const List<String> authMethods = [
+    'assets/images/auth/google.svg',
+    'assets/images/auth/facebook.svg',
+    '',
+  ];
+  static const String appLogo = 'assets/images/app/appLogo.png';
+  static const List<String> homeAppBar = [
+    'assets/images/home/date-range-svgrepo-com.svg',
+    'assets/images/home/bell-svgrepo-com.svg',
+  ];
+  static const String user = 'assets/images/home/user.jpeg';
+  static const String study = 'assets/images/home/study.svg';
+  static const List<String> englishCourse = [
+    'assets/images/english_course/dog.png',
+  ];
+  static const String welcome = 'assets/images/home/Welcome.json';
+  static const String success = 'assets/images/home/success.json';
+  // static const String = '';
+  // static const String = '';
+  // static const String = '';
+  // static const String = '';
+  // static const String = '';
+  // static const String = '';
+}
---
+++ lib/core/utiles/style/font_Styles/app_fonts.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AppFonts {
+  static const TextStyle f30w600black = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'VariableFont_opsz',
+  );
+  static const TextStyle f25w500white = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+    fontFamily: 'VariableFont_opsz',
+  );
+  static const TextStyle f17w500white = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+    fontFamily: 'VariableFont_opsz',
+  );
+  static const TextStyle f16w400gray = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.grey,
+  );
+  static const TextStyle f16w400black = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+  static const TextStyle f14w400gray = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.grey,
+  );
+  static const TextStyle hinterror = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Colors.red,
+  );
+}
