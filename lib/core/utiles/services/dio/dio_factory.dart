59299e307b00a3a7f1f9606f1bae13a96923cccc
mhmd <mhmdlotdy368@gmail.com>
2025-09-06 03:53:33 +0300

    add dioFactory

---
+++ lib/core/utiles/services/dio/dio_factory.dart
@@ -0,0 +1,38 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class DioFactory {
+  DioFactory._();
+
+  static Dio? dio;
+
+  static get context => BuildContext;
+
+  static Dio getDio() {
+    const timeOut = Duration(seconds: 30);
+
+    if (dio == null) {
+      dio = Dio();
+      dio!
+        ..options.connectTimeout = timeOut
+        ..options.receiveTimeout = timeOut;
+      addDioInterceptor();
+      return dio!;
+    } else {
+      return dio!;
+    }
+  }
+
+  static void addDioInterceptor() {
+    dio?.interceptors.add(PrettyDioLogger(compact: false));
+    dio?.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          return handler.next(options);
+        },
+        onError: (error, handler) async {},
+      ),
+    );
+  }
+}
